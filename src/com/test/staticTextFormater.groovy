1a43967e3f13bf2c4972a8c89078c6a2e7559666
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-02 22:15:39 +0200

    Update staticTextFormater.groovy

--- src/com/test/staticTextFormater.groovy
+++ src/com/test/staticTextFormater.groovy
@@ -1,17 +1,12 @@
 package com.test;
 
 class textFormater { 
-  
-  def script
-	staticTextFormater(script) {
-		this.script = script
-	}
-	
-  	static void printFormat(String intext, String color) {   
+   
+  	static void printFormat(script, String intext, String color) {   
     
-        String result_in = "\033[31;42m"
-        String result_out = "\033[0m" 
-        script.echo   result_in + "${intext}" + result_out  	
+		String result_in = "\033[31;42m"
+		String result_out = "\033[0m" 
+		script.echo   result_in + "${intext}" + result_out  	
 		
   	}  
   
