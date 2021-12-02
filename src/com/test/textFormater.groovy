862eb2c60e98f66975239d4271d3fc216c0f9fc6
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-02 21:56:27 +0200

    Update textFormater.groovy

--- src/com/test/textFormater.groovy
+++ src/com/test/textFormater.groovy
@@ -8,12 +8,9 @@ class textFormater {
 	
   	void printFormat(String intext, String color) {   
 		
-		result_in = "\033[31;42m"
-		result_out = "\033[0m"
-
-		result_in = "\033[31;42m"
-		result_out = "\033[0m"
-		script.echo   "${result_in} ${intext} ${result_out}"  	
+		String result_in = "\033[31;42m"
+		String result_out = "\033[0m" 
+		script.echo   result_in + "${intext}" + result_out  	
 		
   	}  
   
