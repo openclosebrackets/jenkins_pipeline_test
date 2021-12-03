e88e719c04db8ff0997196f24cbe0016b1ecc5bc
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-04 00:56:03 +0200

    Update readjs_static.groovy

--- src/com/test/readjs_static.groovy
+++ src/com/test/readjs_static.groovy
@@ -2,13 +2,19 @@ package com.test;
 import groovy.json.JsonSlurperClassic
 
 class readjs_static { 
-
+  
+  static void read_data_static(String jsonFile)
+    {
+         
+    }
+  
+/*
   	static def read_data_static(String jsonFile)
     {
         def json = libraryResource jsonFile                    
         def data = new JsonSlurperClassic().parseText(json)
         return data
     }
- 
+ */
   
 }
