4c1ba86ab114ba0a7f12705d6d201aae311f641b
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-04 00:41:15 +0200

    Update readjs.groovy

--- src/com/test/readjs.groovy
+++ src/com/test/readjs.groovy
@@ -1,7 +1,7 @@
 package com.test;
 import groovy.json.JsonSlurperClassic
 
-def static read_data(String jsonFile)
+static def read_data(String jsonFile)
 {
     def json = libraryResource jsonFile                    
     def data = new JsonSlurperClassic().parseText(json)
