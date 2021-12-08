786a1de1958a20c844b679e693ccbdb32302f27e
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-08 21:26:19 +0200

    Update BashCommandsEmulator.groovy

--- src/com/test/BashCommandsEmulator.groovy
+++ src/com/test/BashCommandsEmulator.groovy
@@ -12,5 +12,11 @@ class BashCommandsEmulator {
     {
         this.script.echo text 
     }
-    
+ 
+    // echo("text1 ","text 2 ")
+    void echo(String... texts)
+    {
+        this.script.echo texts.join("")
+    } 
+ 
 }
