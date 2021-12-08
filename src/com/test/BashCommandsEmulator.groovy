16ae18cf4a8f2bf94ab1c1e25fd5b738c747ceaf
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-08 23:30:25 +0200

    Update BashCommandsEmulator.groovy

--- src/com/test/BashCommandsEmulator.groovy
+++ src/com/test/BashCommandsEmulator.groovy
@@ -2,7 +2,14 @@ package com.test
 
 class BashCommandsEmulator {
     
-    Map colorTable = ["black":"30","red":"31"]
+    private Map colorTable = ["black":30,
+                              "red":31,
+                              "green":32,
+                              "yellow":33,
+                              "blue":34,
+                              "magenta":35,
+                              "cyan":36,
+                              "white":37]
     def script
     
     BashCommandsEmulator(script)
