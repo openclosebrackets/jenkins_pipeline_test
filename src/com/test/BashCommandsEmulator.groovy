0a70759e965792f05c999d572b52887064d9f1bb
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-08 21:52:56 +0200

    Update BashCommandsEmulator.groovy

--- src/com/test/BashCommandsEmulator.groovy
+++ src/com/test/BashCommandsEmulator.groovy
@@ -2,6 +2,9 @@ package com.test
 
 class BashCommandsEmulator {
     def script
+    String ansi_start = "\033"
+    String ansi_stop = "\033[0m"
+    
     
     BashCommandsEmulator(script)
     {
@@ -11,7 +14,13 @@ class BashCommandsEmulator {
     // echo("text1 ","text 2 ")
     void echo(String... texts)
     {
-        this.script.echo texts.join("")
+        String color = "31"
+        String bold = "1"
+        String underline = "4"
+        //"\033[31;1;4mHello\033[0m"
+        String str = "${this.ansi_start}[${color};${bold};${underline}m${texts.join("")}${this.ansi_stop}"
+        //String str =  + "[" + color + ";" + bold + ";" + underline + "m" + texts.join("") + this.ansi_stop
+        this.script.echo str
     } 
  
 }
