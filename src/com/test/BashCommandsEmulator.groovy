35a32f444940be982ce2f275a74137184037848b
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-08 22:13:28 +0200

    Update BashCommandsEmulator.groovy

--- src/com/test/BashCommandsEmulator.groovy
+++ src/com/test/BashCommandsEmulator.groovy
@@ -4,7 +4,6 @@ class BashCommandsEmulator {
     def script
     String ansi_start = "\033"
     String ansi_stop = "\033[0m"
-    Map format = [color:"30",bold:"0", underline:"4"]
     
     
     BashCommandsEmulator(script)
@@ -16,11 +15,25 @@ class BashCommandsEmulator {
     void echo(String... texts)
     {
         String color = "31"
-        String bold = "1"
-        String underline = "4"
+        boolean bold = false
+        boolean underline = false
+        
+        String str = "${this.ansi_start}[${color}"
+        if (bold)
+        {
+            str += ";1"
+        }
+        if (underline)
+        {
+            str += ";4"
+        }
+        str += "m${texts.join("")}${this.ansi_stop}" 
+        
+        
+        //String underline = "4"
         
         //example: "\033[31;1;4mHello\033[0m"
-        String str = "${this.ansi_start}[${this.format.color};${this.format.bold};${this.format.underline}m${texts.join("")}${this.ansi_stop}"
+        //String str = "${this.ansi_start}[${color};${bold};${underline}m${texts.join("")}${this.ansi_stop}"
         
         this.script.echo str
     } 
