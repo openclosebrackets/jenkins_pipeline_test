840edc58993c0525ad673ed49ea88f73d8275f22
openclosebrackets <81322435+openclosebrackets@users.noreply.github.com>
2021-12-08 23:55:46 +0200

    Update BashCommandsEmulator.groovy

--- src/com/test/BashCommandsEmulator.groovy
+++ src/com/test/BashCommandsEmulator.groovy
@@ -23,22 +23,30 @@ class BashCommandsEmulator {
         this.script.echo texts.join("")
     } 
     
-    // echo([color:"red",bold: true, underline: true],"text1 ","text 2 ")
+    // Example echo([color:"red",bold: true, underline: true],"text1 ","text 2 ")
     void echo(Map format, String... texts)
     {
+        // check if user gives format keys, if not set default
         if (format.color == null) format.color = "black"
         if (format.bold == null) format.bold = false
         if (format.underline == null) format.underline = false
         
+        // you can now give option as "Red","red", "REd",...
+        format.color = format.color.toLowerCase()
+        
+        // if color is not in color table, set to black
+        if (!(format.color in colorTable.keySet())) format.color = "black"
+        
+        // ansi coloring 
         String str = "\033[${colorTable[format.color as String]}"
-        if (format.bold)
-        {
-            str += ";1"
-        }
-        if (format.underline)
-        {
-            str += ";4"
-        }
+        
+        if (format.bold) str+=";1"
+        
+        if (format.backgroundColor in colorTable.keySet())) str+=";${colorTable[format.color as String] + 10}"
+        
+        if (format.underline) str+=";4"
+        
+        //final string to echo
         str += "m${texts.join("")}\033[0m"  
         
         this.script.echo str
